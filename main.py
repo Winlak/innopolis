e9c31adfb18444e0216d5fa3326d967586443ac3
Winlak <leprikontrue@gmail.com>
2024-04-27 17:47:37 +0300

    one comm

---
+++ main.py
@@ -0,0 +1,27 @@
+#
+# x,y = 1,2
+# def one():
+#     global x,y
+#     d = x + y
+#     return d
+#
+# def two():
+#     return one()
+#
+# print(two())
+
+
+
+
+x,y = 1,2 #global
+def one():
+    global x,y
+    d = x + y
+    #d - local
+    print(d)
+
+one()
+
+def two():
+    a,b = 3,4 #local
+    print(a+b)
